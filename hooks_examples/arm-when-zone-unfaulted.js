cc1b72c6183b132986d787a4c7e924fa6c93b0fb
Brady Holt <brady.holt@gmail.com>
2014-11-14 06:57:44 -0600

    Fix syntax issue with arm-when-zone-unfaulted.js

--- hooks_examples/arm-when-zone-unfaulted.js
+++ hooks_examples/arm-when-zone-unfaulted.js
@@ -3,7 +3,7 @@ module.exports = function (ICM) {
 		var day=new Date().getDay();
 		var hour=new Date().getHours();
 		if(zoneNumber==18 
-			&& !isFaulted ){
+			&& !isFaulted 
 			&& (day >= 1 && day <= 5)
 			&& (hour >= 6 && hour <= 9)){
 		    ICM.executeCommand('F1'); //arm STAY
